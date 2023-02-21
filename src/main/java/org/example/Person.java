0a1ecc0728e89dce5ddc143c442022af92dd64b3
June <june122@inbox.ru>
2023-02-21 21:08:30 +0300

    hw2

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,21 +1,24 @@
 package org.example;
+import org.example.transport.Transport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Person {
-    private Car car;
+   private Transport transport;
 
-    public Person() {
+   @Autowired
+    public Person(@Qualifier("car") Transport transport ) {
+        this.transport = transport;
     }
 
-    public Person( Car car ) {
-        this.car = car;
+    public void setTransport( Transport transport) {
+        this.transport = transport;
     }
 
-    public void setCar( Car car ) {
-        this.car = car;
-    }
-
-    public void sitInCar(){
+    public void sitInTransport(){
         System.out.println("Владелец сел в автомобиль");
-        car.go();
+        transport.go();
     }
 }
